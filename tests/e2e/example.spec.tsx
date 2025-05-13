52c308239a0b767913a3b7eca6ad0ca081d282bd
yanicorn3000 <yanadziabiola@gmail.com>
2025-05-13 18:08:34 +0200

    test fixes

--- tests/e2e/example.spec.tsx
+++ tests/e2e/example.spec.tsx
@@ -4,8 +4,3 @@ test("strona główna ma poprawny tytuł", async ({ page }) => {
   await page.goto("http://localhost:5173/outfit");
   await expect(page).toHaveTitle(/the Outfit/i);
 });
-
-test("strona główna ma poprawny tytuł", async ({ page }) => {
-  await page.goto("http://localhost:5173/outfit");
-  await expect(page).toHaveTitle(/the Outfit/i);
-});
