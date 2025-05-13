418e855e4124e79dc8654124b1304ebf765b457e
yanicorn3000 <yanadziabiola@gmail.com>
2025-05-13 18:05:12 +0200

    test fixes

--- tests/e2e/example.spec.tsx
+++ tests/e2e/example.spec.tsx
@@ -4,3 +4,8 @@ test("strona główna ma poprawny tytuł", async ({ page }) => {
   await page.goto("http://localhost:5173/outfit");
   await expect(page).toHaveTitle(/the Outfit/i);
 });
+
+test("strona główna ma poprawny tytuł", async ({ page }) => {
+  await page.goto("http://localhost:5173/outfit");
+  await expect(page).toHaveTitle(/the Outfit/i);
+});
